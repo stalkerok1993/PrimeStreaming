50a6324e271456907dc0e81d8c5b31967f562724
Serhii Bihun <s.bihun93@gmail.com>
2021-07-12 23:20:13 +0300

    Initial commit.

---
+++ PrimeStreaming/Primes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeStreaming
+{
+    public class Primes
+    {
+        public IEnumerable<int> Stream()
+        {
+            return new List<int> { 2, 3, 5, 7, 11, 13, 17 };
+        }
+    }
+}
